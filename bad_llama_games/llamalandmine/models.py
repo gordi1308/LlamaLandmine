cf25d32ba080d1b7c22d68873a3f517b0ff2e98e
Tomko07 <clemence.brival@gmail.com>
2016-02-26 17:11:59 +0000

    Created models for the database

--- bad_llama_games/llamalandmine/models.py
+++ bad_llama_games/llamalandmine/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
-# Create your models here.
+
+# Model to represent a badge.
+class Badge(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    tier = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name + " " + self.tier
+
+
+# Model to represent a registered user.
+# (Not called User to differentiate from the django User class)
+class RegisteredUser(models.Model):
+    # Django User object with the default user attributes.
+    user = models.OneToOneField(User)
+    picture = models.ImageField(blank=True)
+    games_played_easy = models.IntegerField(default=0)
+    games_played_medium = models.IntegerField(default=0)
+    games_played_hard = models.IntegerField(default=0)
+
+    # A user can earn many badges. A badge can be earned by many users.
+    earned_badges = models.ManyToManyField(Badge)
+    # A user can send friend requests to many users. A user can receive friends requests from many users.
+    friends = models.ManyToManyField("self", related_name="friends")
+
+    def __unicode__(self):
+        return self.user.username
+
+
+# Model to represent a game.
+class Game(models.Model):
+    level = models.CharField(max_length=6)
+    was_won = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
+    date = models.DateField(default=date.today)
+    time_taken = models.IntegerField(default=0)
+
+    # A user can play one game at time. A game can be played by one user.
+    user = models.OneToOneField(RegisteredUser, related_name="current_game")
+
+
+# Model to represent a challenge.
+class Challenge(models.Model):
+    score_to_beat = models.IntegerField()
+    accepted = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+    remaining_attempts = models.IntegerField(default=5)
+
+    # A challenge can be made by one user. A user can make several games
+    challenger = models.ForeignKey(RegisteredUser, related_name='challenges_created')
+    # For now, only one user can be challenged at a time.
+    challenged_user = models.ForeignKey(RegisteredUser, related_name="challenged_received")
