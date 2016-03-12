d0c56aef30d4490d33696c587590ea0d9a943c39
Tomko07 <clemence.brival@gmail.com>
2016-03-12 18:27:23 +0000

    Created basic views

--- bad_llama_games/llamalandmine/views.py
+++ bad_llama_games/llamalandmine/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def register(request):
+    return render(request, 'register.html', {})
+
+
+def game(request):
+    return render(request, 'game.html', {})
+
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+
+def leaderboard(request):
+    return render(request, 'leaderboard.html', {})
+
+
+def how_to(request):
+    return render(request, 'howto.html', {})
+
+
+def game_over(request):
+    return render(request, 'game_over.html', {})
+
+
+def edit_profile(request):
+    return render(request, 'editprof.html', {})
