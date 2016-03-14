6b7b12e5d98dec989f853d878407e38e60eb8f9e
GreggWighton <greggwighton@gmail.com>
2016-03-14 14:26:43 +0000

    That's me (FINALLY) started the views.py :)

--- bad_llama_games/llamalandmine/urls.py
+++ bad_llama_games/llamalandmine/urls.py
@@ -4,4 +4,10 @@ from llamalandmine import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='home'),
-                       url(r'^register/$', views.register, name='register'),)
+                       url(r'^register/$', views.register, name='register'),
+                       url(r'game/$',views.game, name='game'),
+                       url(r'profile/$',views.profile, name='profile'),
+                       url(r'leaderboard/$',views.leaderboard, name='leaderboard'),
+                       url(r'how_to/$',views.how_to, name='how_to'),
+                       url(r'game_over',views.game_over, name='game_over'),
+                       url(r'edit_profile',views.edit_profile, name='edit_profile'))
