f532070a48c021f08e7753ee061e807eb1d61955
Tomko07 <clemence.brival@gmail.com>
2016-03-17 21:11:43 +0000

    Added urls to handle game

--- bad_llama_games/llamalandmine/urls.py
+++ bad_llama_games/llamalandmine/urls.py
@@ -5,9 +5,12 @@ from llamalandmine import views
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='home'),
                        url(r'^register/$', views.register, name='register'),
-                       url(r'^game/$',views.game, name='game'),
+                       url(r'^play/$',views.play, name='play'),
+                       url(r'^game/(?P<level>[\w\-]+)/$', views.game, name='game'),
+                       url(r'^get_grid_data/$', views.get_grid_data, name='get_grid_data'),
                        url(r'^profile/$',views.profile, name='profile'),
                        url(r'^leaderboard/$',views.leaderboard, name='leaderboard'),
                        url(r'^how_to/$',views.how_to, name='how_to'),
                        url(r'^userlogout/$',views.userlogout, name='userlogout'),
-                       url(r'^game_over',views.game_over, name='game_over'),)
+                       url(r'^game_over/',views.game_over, name='game_over'),
+)
