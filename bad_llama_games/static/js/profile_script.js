2d0fc39263cdec960935f671629ebc778643549e
Tomko07 <Tomko07@users.noreply.github.com>
2016-03-20 19:06:18 +0000

    Profile script and game script not user post_request_handler

--- bad_llama_games/static/js/profile_script.js
+++ bad_llama_games/static/js/profile_script.js
@@ -11,35 +11,11 @@ $(document).ready(function() {
 
 function handle_request(from, accept){
 
-    function getCookie(name) {
-        var cookieValue = null;
+    var view = '/llamalandmine/handle_requests/';
+    var data = {from: from, accept: accept};
+    var action = function() {
+        location.reload(true);
+    };
 
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    var csrftoken = getCookie('csrftoken');
-
-    $.ajax({
-        type: 'POST',
-        url: '/llamalandmine/handle_requests/',
-        data: {
-            from: from,
-            accept: accept,
-            csrfmiddlewaretoken: csrftoken
-        },
-        success: function() {
-            location.reload(true);
-        }
-    });
+    post_request_handler(view, data, action);
 }
