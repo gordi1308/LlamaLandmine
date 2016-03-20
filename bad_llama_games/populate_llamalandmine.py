cfa7e250479d7ea87d992a9e0999f6f5cc79f48d
gordi1308 <gordon_mccolm@hotmail.com>
2016-03-20 18:13:55 +0000

    Badge images added

--- bad_llama_games/populate_llamalandmine.py
+++ bad_llama_games/populate_llamalandmine.py
@@ -20,111 +20,111 @@ def populate():
 
     play_one_game = add_badge(name="Of all the games, on all the websites, in all the world, you logged on to mine",
                               description="Play your first game.",
-                              tier=1)
+                              tier=1, image="of_all_the_games.png")
     add_badge(name="At first you had my curiosity, now you have my attention",
               description="Play 25 games.",
-              tier=2)
+              tier=2, image="you_had_my_curiosity.png")
     add_badge(name="Addicted",
               description="Play 50 games.",
-              tier=3)
+              tier=3, image="addicted.png")
     play_one_easy_game = add_badge(name="Baby steps",
                                    description="Play a game on Easy difficulty.",
-                                   tier=1)
+                                   tier=1, image="baby_steps.png")
     play_one_normal_game = add_badge(name="There's always a bigger fish",
                                      description="Play a game on Normal difficulty.",
-                                     tier=2)
+                                     tier=2, image="always_a_bigger_fish.png")
     play_one_hard_game = add_badge(name="Now this is Llama Landmine!",
                                    description="Play a game on Hard difficulty.",
-                                   tier=3)
+                                   tier=3, image="now_this_is.png")
 
     # badges related to wins
     print "Adding badges related to wins..."
 
     win_one_game = add_badge(name="Chicken Dinner",
                              description="Win your first game.",
-              tier=1)
+              tier=1, image="chicken_dinner.png")
     add_badge(name="You wanna be a big cop in a small town?",
               description="Win 10 games on Easy difficulty.",
-              tier=1)
+              tier=1, image="big_cop.png")
     add_badge(name="Like procuring sweets from an infant",
               description="Win 25 games on Easy difficulty.",
-              tier=1)
+              tier=1, image="like_procuring_sweets.png")
     add_badge(name="One small step for Man...",
               description="Win 10 games on Normal difficulty.",
-              tier=2)
+              tier=2, image="one_small_step.png")
     add_badge(name="That'll do llama",
               description="Win 25 games on Normal difficulty.",
-              tier=2)
+              tier=2, image="thatll_do_llama.png")
     add_badge(name="One giant leap for Llama-kind",
               description="Win 10 games on Hard difficulty.",
-              tier=3)
+              tier=3, image="one_giant_leap.png")
     add_badge(name="The Llama Whisperer",
               description="Win 25 games on Hard difficulty",
-              tier=3)
+              tier=3, image="llama_whisperer.png")
 
     # badges related to challenges
     print "Adding badges related to challenges..."
 
     add_badge(name="Great kid, don't get cocky",
               description="Win your first Challenge.",
-              tier=1)
+              tier=1, image="great_kid.png")
     add_badge(name="King of the Hill",
               description="Win 5 Challenges.",
-              tier=2)
+              tier=2, image="king_hill.png")
     add_badge(name="King of the Mountain",
               description="Win 15 Challenges.",
-              tier=3)
+              tier=3, image="king_mountain.png")
     add_badge(name="Playground Bully!",
               description="Beat the same user 5 times in 5 Challenges.",
-              tier=2)
+              tier=2, image="bully.png")
     add_badge(name="It's a trap!",
               description="Receive 5 challenges.",
-              tier=1)
+              tier=1, image="trap.png")
     add_badge(name="I swear by my pretty floral bonnet I will end you",
               description="Accept a Challenge from a friend with a game on Easy difficulty.",
-              tier=1)
+              tier=1, image="floral_bonnet.png")
     add_badge(name="Handbags at dawn",
               description="Accept a challenge from a friend with a game on Normal difficulty",
-              tier=1)
+              tier=1, image="handbags.png")
     add_badge(name="It's a bold strategy Cotton!",
               description="Accept a Challenge from a friend with a game on Hard difficulty.",
-              tier=1)
+              tier=1, image="bold_strategy.png")
 
     # badges related to friends
     print "Adding badges related to friends..."
 
     add_badge(name="Phone a Friend",
               description="Invite a friend to Llama Landmine.",
-              tier=1)
+              tier=1, image="phone.png")
     add_badge(name="Gondor calls for aid!",
               description="Invite 5 friends to Llama Landmine.",
-              tier=2)
+              tier=2, image="gondor.png")
 
     # badges related to login
     print "Adding badges related to login..."
 
     add_badge(name="Hello Again!",
               description="Log in 2 days in a row.",
-              tier=1)
+              tier=1, image="hello.png")
     add_badge(name="Mambo Number 5",
               description="Log in 5 days in a row.",
-              tier=2)
+              tier=2, image="mambo.png")
     add_badge(name="Gotta save 'em all",
               description="Log in 10 days in a row... Don't you have anything better to do?",
-              tier=3)
+              tier=3, image="save.png")
 
     # Misc badges
     print "Adding misc badges..."
 
     add_badge(name="Ouchtown population you bro!",
               description="Click on your first mine.",
-              tier=1)
+              tier=1, image="ouchtown.png")
     #add_badge(name="...At least I've got chicken",
     #          description="Lose a game within 2 seconds with more than 5 clicks.",
-    #          tier=2)
+    #          tier=2, image="chicken.png")
     add_badge(name="Collect all the badges!",
               description="Why are you even reading this...",
-              tier=3)
+              tier=3, image="collected_all_badges.png")
 
     print "Adding users and games..."
 
@@ -253,17 +253,19 @@ def populate():
         print "\tFriends: " + str(user.friends.count())
 
 
-def add_badge(name, description, tier):
+def add_badge(name, description, tier, image):
     """Creates a new badge and adds it to the db.
     :param name: badge name
     :param description: badge description
     :param tier: badge tier
+    :param image: badge image
     :return: the created Badge object
     """
 
     badge = Badge.objects.get_or_create(name=name)[0]
     badge.description = description
     badge.tier = tier
+    badge.icon = image
     badge.save()
     return badge
 
